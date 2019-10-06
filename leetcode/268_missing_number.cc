6b11e79700309fda87e0ff77c76823979c850d0f
liurui <liu_rui2727@163.com>
2019-10-06 16:38:05 +0800

    update some solutions

---
+++ leetcode/268_missing_number.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int res = nums.size();
+       
+        for (int i = 0; i < nums.size(); ++i) {
+            res ^= nums[i] ^ i;
+        }
+        
+        return res;
+    }
+};
