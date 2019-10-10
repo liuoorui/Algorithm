dea3eb7d83cb6abd14abb4149e5a93aeee17fef4
liurui <liu_rui2727@163.com>
2019-10-10 23:51:03 +0800

    update some solutions

---
+++ leetcode/307_range_sum_query_mutable.cc
@@ -0,0 +1,44 @@
+#include <vector>
+#include <iostream>
+
+class NumArray {
+public:
+    NumArray(vector<int>& nums) 
+    : treeNums_(nums.size() + 1, 0)
+    {
+        for (size_t i = 0; i < nums.size(); ++i) {
+            update(i, nums[i]);
+        }
+    }
+    
+    void update(int i, int val) {
+        int idx = i + 1;
+        int num = getPrefixSum(idx) - getPrefixSum(idx - 1);
+        int diff = val - num;
+        
+        for (; idx < treeNums_.size(); idx += lowbit(idx)) {
+            treeNums_[idx] += diff;
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return getPrefixSum(j + 1) - getPrefixSum(i);
+    }
+private:
+    vector<int> treeNums_;
+    // 可以将nums也存起来，空间换取时间，以减少update时候计算num的时间开销。
+    
+    inline int lowbit(int idx) {
+        return idx & (-idx);
+    }
+    
+    inline int getPrefixSum(int idx) {
+        int prefix = 0;
+        
+        for (; idx > 0; idx -= lowbit(idx)) {
+            prefix += treeNums_[idx];
+        }
+        
+        return prefix;
+    }
+};
